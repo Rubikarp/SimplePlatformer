c78bb3ad9b3b337e1c0f7a979c626313479601cb
Arthur Deleye <arthurdeleye@gmail.com>
2025-10-24 00:48:56 +0200

    [UPD] Improve Controller

--- Assets/Scripts/Controler/PlayerJump.cs
+++ Assets/Scripts/Controler/PlayerJump.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
@@ -18,73 +19,46 @@ public class PlayerJump : MonoBehaviour
 
     [Header("Current State")]
     public JumpState currentState;
-    public JumpState CurrentState
-    {
-        get => currentState;
-        private set
-        {
-            if (currentState == value) return;
-
-            //When Exiting 
-            switch (currentState)
-            {
-                case JumpState.Grounded:
-                    break;
-                case JumpState.Jumping:
-                    break;
-                case JumpState.Falling:
-                    break;
-            }
-
-            //When Entering State
-            switch (value)
-            {
-                case JumpState.Grounded:
-
-                    break;
-                case JumpState.Jumping:
-
-                    break;
-                case JumpState.Falling:
-
-                    break;
-            }
-
-            currentState = value;
-        }
-    }
-    private bool IsGrounded => CurrentState == JumpState.Grounded;
 
     [Header("Jump Settings")]
-    [SerializeField, Tooltip("Maximum jump height"), Range(2f, 5.5f)]
-    public float jumpHeight = 3f;
-    [SerializeField, Tooltip("How long it takes to reach that height before coming back down"), Range(0.2f, 1.25f)]
-    public float timeToReachApex = 0.5f;
+    [SerializeField, Range(2f, 5.5f)]
+    private float jumpHeight = 3f;
+    [SerializeField, Range(0.2f, 1.25f)]
+    private float timeToReachApex = 0.5f;
 
     [Header("Settings")]
-    [SerializeField, Range(1f, 10f), Tooltip("Gravity multiplier to apply when coming down")]
-    public float groundGravityMultiplier = 1f;
-    [SerializeField, Range(0f, 5f)]
-    [Tooltip("Gravity multiplier to apply when going up")]
-    public float jumpGravityMultiplier = .95f;
     [SerializeField, Range(1f, 10f)]
-    [Tooltip("Gravity multiplier to apply when coming down")]
-    public float fallGravityMultiplier = 1.75f;
+    private float groundGravityMultiplier = 1f;
+    [SerializeField, Range(0f, 5f)]
+    private float jumpGravityMultiplier = .95f;
+    [SerializeField, Range(1f, 10f)]
+    private float fallGravityMultiplier = 1.75f;
     [Space]
-    [SerializeField, Range(1f, 10f), Tooltip("The fastest speed the character can fall")]
-    public float maxFallSpeed = 12f;
+    [SerializeField, Range(1f, 10f)]
+    private float maxFallSpeed = 12f;
 
     [Header("Options Analogic")]
-    [SerializeField, Tooltip("Should the character drop when you let go of jump?")]
-    public bool isJumpAnalogic;
-    [SerializeField, Tooltip("Gravity multiplier when you let go of jump"), Range(1f, 10f)]
-    public float jumpCutOffGravityMultiplier = 1.75f;
+    public bool isJumpAnalogic = false;
+    [SerializeField, Range(1f, 10f)]
+    private float jumpCutOffGravityMultiplier = 1.75f;
 
-    [Header("Calculations")]
-    [SerializeField] private bool desiredJump;
-    [SerializeField] private bool pressingJump;
-    [SerializeField] private float initialJumpForce;
-    [SerializeField] private float currentGravityMultiplier;
+    [Header("Options Coyotee Time")]
+    public bool useCoyoteeTime = false;
+    [SerializeField, Range(0f, 1f)]
+    private float coyoteeTimeWindow = .2f;
+
+    [Header("Options Jump Buffer")]
+    public bool useJumpBuffer = false;
+    [SerializeField, Range(0f, 1f)]
+    private float jumpBufferTimeWindow = .2f;
+
+    [Header("Info")]
+    private bool isPressingJump;
+    [Space]
+    private float initialJumpForce;
+    private float currentGravityMultiplier;
+    private float leaveGroundMomment;
+    private float lastJumpBufferMoment;
 
     [Header("Events")]
     public UnityEvent onJump;
@@ -94,90 +68,19 @@ public class PlayerJump : MonoBehaviour
         // GetComponent() permet de rechercher sur son objet s'il y a un composant Rigidbody2D
         body = GetComponent<Rigidbody2D>();
         body.gravityScale = groundGravityMultiplier;
+
+        playerInfo.onLeaveGround.AddListener(OnLeaveGround);
+        playerInfo.onTouchGround.AddListener(OnTouchGround);
     }
+
     private void FixedUpdate()
     {
-        CalculateGravityScale();
+        if (currentState == JumpState.Jumping && body.linearVelocity.y < 0) currentState = JumpState.Falling;
 
-        //Keep trying to do a jump, for as long as desiredJump is true
-        if (desiredJump)
-        {
-            DoJump();
-
-            //Skip gravity calculations this frame, so currentlyJumping doesn't turn off
-            //This makes sure you can't do the coyote time double jump bug
-            return;
-        }
-
-        CheckPlayerState();
-
-        //Set the character's Rigidbody's velocity
-        //But clamp the Y variable within the bounds of the speed limit, for the terminal velocity assist option
-        body.linearVelocityY = Mathf.Max(body.linearVelocityY, -maxFallSpeed);
-    }
-
-    public void OnJump(InputAction.CallbackContext context)
-    {
-        if (!playerInfo.CharacterCanMove) return;
-
-        //When we press the jump button, tell the script that we desire a jump.
-        //Also, use the started and canceled contexts to know if we're currently holding the button
-        if (context.started)
-        {
-            desiredJump = true;
-            pressingJump = true;
-        }
-        if (context.canceled)
-        {
-            pressingJump = false;
-        }
-    }
-
-    private void DoJump()
-    {
-        desiredJump = false;
-        //Create the jump, provided we are on the ground, in coyote time, or have a double jump available
-        if (!IsGrounded)
-        {
-            //Put in Buffer ?
-            return;
-        }
-
-        CurrentState = JumpState.Jumping;
-
-        // Compute initial velocity to reach apex at timeToApex:
-        // InitialVelocity = GravityScale * TimeToReachApex
-        initialJumpForce = GetGravityConstant() * timeToReachApex;
-        body.linearVelocityY = initialJumpForce;
-
-        onJump?.Invoke();
-    }
-    private void CheckPlayerState()
-    {
-        switch (CurrentState)
-        {
-            case JumpState.Grounded: return;
-            case JumpState.Jumping:
-                if (body.linearVelocity.y < 0) CurrentState = JumpState.Falling;
-                break;
-            case JumpState.Falling:
-                if (playerInfo.IsGrounded) CurrentState = JumpState.Grounded;
-                break;
-        }
-    }
-    private float GetGravityConstant()
-    {
-        //Formule de physique :
-        //Hauteur = (1/2) * Gravité * Temps²
-        //Donc Gravité = (2 * Hauteur) / Temps²
-        return (2f * jumpHeight) / Mathf.Pow(timeToReachApex, 2f);
-    }
-    private void CalculateGravityScale()
-    {
         var newGravityScale = GetGravityConstant();
-        newGravityScale *= CurrentState switch
+        newGravityScale *= currentState switch
         {
-            JumpState.Jumping => (isJumpAnalogic && !pressingJump) ? jumpGravityMultiplier * jumpCutOffGravityMultiplier : jumpGravityMultiplier,
+            JumpState.Jumping => (isJumpAnalogic && !isPressingJump) ? jumpGravityMultiplier * jumpCutOffGravityMultiplier : jumpGravityMultiplier,
             JumpState.Falling => fallGravityMultiplier,
             JumpState.Grounded => groundGravityMultiplier,
             _ => groundGravityMultiplier,
@@ -186,8 +89,79 @@ public class PlayerJump : MonoBehaviour
         //Gravité = gravityForce / Physics2D.gravity.y
         //Et comme Physics2D.gravity.y est négatif, on l'inverse en mettant un - devant
         newGravityScale /= -Physics2D.gravity.y;
-
         currentGravityMultiplier = newGravityScale;
         body.gravityScale = currentGravityMultiplier;
+
+        //Limite la velocité terminal (la vitesse de chute maximum)
+        body.linearVelocityY = Mathf.Max(body.linearVelocityY, -maxFallSpeed);
     }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (!playerInfo.CharacterCanMove) return;
+
+        if (context.started) TryToJump();
+    }
+
+    private void OnTouchGround()
+    {
+        if (useJumpBuffer)
+        {
+            bool inbufferWindow = (Time.time - lastJumpBufferMoment) < jumpBufferTimeWindow;
+            if (inbufferWindow) Jump();
+        }
+
+        currentState = JumpState.Grounded;
+    }
+    private void OnLeaveGround()
+    {
+        leaveGroundMomment = Time.time;
+
+        if (body.linearVelocity.y < 0f)
+        {
+            currentState = JumpState.Falling;
+        }
+        else
+        {
+            currentState = JumpState.Jumping;
+        }
+    }
+
+    private void TryToJump()
+    {
+        if (!playerInfo.CharacterCanMove) return;
+
+        bool validCoyoteeTime = useCoyoteeTime && (Time.time - leaveGroundMomment) < coyoteeTimeWindow;
+        //Create the jump, provided we are on the ground, in coyote time, or have a double jump available
+        if (!playerInfo.IsGrounded && !validCoyoteeTime)
+        {
+            //Put in Jump Buffer if activated
+            if (useJumpBuffer) lastJumpBufferMoment = Time.time;
+            return;
+        }
+
+        Jump();
+    }
+
+    private void Jump()
+    {
+        currentState = JumpState.Jumping;
+        lastJumpBufferMoment = - ;
+
+        // Compute initial velocity to reach apex at timeToApex:
+        // InitialVelocity = GravityScale * TimeToReachApex
+        initialJumpForce = GetGravityConstant() * timeToReachApex;
+        body.linearVelocityY = initialJumpForce;
+
+        onJump?.Invoke();
+    }
+
+    private float GetGravityConstant()
+    {
+        //Formule de physique :
+        //Hauteur = (1/2) * Gravité * Temps²
+        //Donc Gravité = (2 * Hauteur) / Temps²
+        return (2f * jumpHeight) / Mathf.Pow(timeToReachApex, 2f);
+    }
+
 }
